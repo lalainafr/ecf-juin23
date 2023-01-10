13f589c623abf4850adc2b636b423bc166f1b270
lalaina rajaonahsoa <r.lalainafr@gmail.com>
2023-01-10 23:41:54 +0100

    Creating login form - setting boosttrap form theme - flash message - invalid credential langaue - logout - setting login and logout on the header navbar

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    #[Route('/connexion', name: 'app_login')]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+            // get the login error if there is one
+            $error = $authenticationUtils->getLastAuthenticationError();
+
+            // last username entered by the user   
+            $lastUsername = $authenticationUtils->getLastUsername();
+
+            $this->addFlash(
+                'success',
+                'Vous êtes bien connecté',
+            );
+        
+        return $this->render('pages/login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error, 
+        ]);
+    }
+
+    #[Route('/deconnexion', name: 'app_logout')]
+    public function logout()
+    {
+    }
+}
